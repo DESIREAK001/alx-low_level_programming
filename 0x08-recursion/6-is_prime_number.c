0ca32990b19d55742e99ca57a289e884cc0a1a62
DESIREAK001 <desirengonzikabagyo@gmail.com>
2023-10-07 06:06:21 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,21 +3,21 @@ int actual_prime(int n, int i);
 /**
  * is_prime_number - finds the prime number of n
  * @n: number whose prime number is to be found
- * Return: prime number of n
+ * Return: 1 if n is a prime number, 0 if not
  */
 
 int is_prime_number(int n)
 {
 	if (n < 0)
 		return (0);
-	return (actual_prime(n, 0));
+	return (actual_prime(n, n - 1));
 }
 
 /**
  * actual_prime - recurses to find the prime of a number
  * @n: number whose prime is to be found
  * @i: iterator
- * Return: prime number of n
+ * Return: 1 if n is a prime number, 0 if not
  */
 
 int actual_prime(int n, int i)
